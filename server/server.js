ac9aa1d99513ddd87c5b30ac31218ff71072e7dc
Lakusan <11012049@stud.hochschule-heidelberg.de>
2020-09-06 17:15:20 +0200

    Map test

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const server = http.createServer(function (req, res) {
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
-    fs.readFile('./index.html', function (error, data) {
+    fs.readFile('../map.html', function (error, data) {
         if (error) {
             res.writeHead(404);
             res.write('Error: File not Found');
